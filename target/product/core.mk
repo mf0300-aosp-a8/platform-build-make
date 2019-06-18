a333f04f510b57c5d6d26846f5f6285223c6c610
Nick Korotysh <kolchaprogrammer@list.ru>
2019-06-18 12:25:11 +0300

    Re-enable DocumentsUI
    DocumentsUI is simple file manager used to explore external drives
    in various cases, for certificates installation for example.

--- target/product/core.mk
+++ target/product/core.mk
@@ -24,6 +24,7 @@ PRODUCT_PACKAGES += \
     BuiltInPrintService \
     CaptivePortalLogin \
     CertInstaller \
+    DocumentsUI \
     ExternalStorageProvider \
     FusedLocation \
     InputDevices \
